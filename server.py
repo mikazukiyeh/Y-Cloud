508b0607ab7b94baf0e0c95d29f9c43bb7c0e887
Luna Yeh <131629497+mikazukiyeh@users.noreply.github.com>
2023-07-10 13:11:42 +0800

    Update server.py

--- server.py
+++ server.py
@@ -2,11 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/" , methods = ["GET"])
+@app.route("/", methods = ["GET"])
 def home():
     return "OK"
 
 if __name__ == "__main__":
-    app.debug = Flase
-    app.run(host = "0.0.0.0" , port = 5001)
-
+    app.debug = False
+    app.run(host = "0.0.0.0", port = 5001)
