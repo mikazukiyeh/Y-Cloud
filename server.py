45acdf3cbcda6bef8e342252fe40d1d1e17de0ef
Luna Yeh <131629497+mikazukiyeh@users.noreply.github.com>
2023-07-13 15:28:20 +0800

    Update server.py
    後端

--- server.py
+++ server.py
@@ -1,20 +1,47 @@
-import json
-from flask import Flask, request, Response
+from flask import request
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+db = SQLAlchemy()
 
 app = Flask(__name__)
 
-@app.route("/", methods = ["GET"])
-def compare():
-    a = int(request.args.get("a"))
-    b = int(request.args.get("b"))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:user1234@db_host:5432/website"
 
-    try:
-        if a > b:
-            return Response(json.dumps({"status":"a>b"}), mimetype="application/json")
-        else:
-            return Response(json.dumps({"status":"a<=b"}), mimetype="application/json")
-    except:
-        return Response(json.dumps({"status":"invalid parameter"}), status = 403, mimetype="application/json")
+db.init_app(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+
+@app.route('/createAll')
+def index():
+    with app.app_context():
+        db.create_all()
+    
+    return "OK"
+
+@app.route("/signup", methods = ["POST"])
+def signup():
+    with open("/app/logs/log.txt","w") as f:
+        f.write("(Backend) Got sigup request ...")
+
+    req_name = request.form.get("name")
+    obj_user = User(name = req_name)
+    db.session.add(obj_user)
+    db.session.commit()
+
+    return "OK"
+
+@app.route("/listAccounts", methods = ["GET"])
+def listAccount():
+    users = User.query.all()
+    list_users = []
+    for obj in users:
+        list_users.append(obj.name)
+
+    return str(list_users)
 
 if __name__ == "__main__":
     app.debug = False
